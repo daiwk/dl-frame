7e84f5845404bcbed114cdbf29f32f2ea70a4027
daiwk <daiwk@foxmail.com>
2019-10-29 00:54:36 +0800

    x

--- python/dl-frame/infer/test.py
+++ python/dl-frame/infer/test.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
 #encoding=utf8
  
-"""
+'''
 FileName: test.py
 Date: 2019-10-28 23:51:36
-"""
+'''
 
 class demoxxx(object):
-    """
+    '''
     demo
-    """
+    '''
     def func_demo(args):
-        """
+        '''
         Args:
             + kargs:dict
         Returns:
             xxx
-        """
+        '''
         pass
