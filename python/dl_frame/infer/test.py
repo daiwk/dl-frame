3081573263b90f33161ac81b153137ff55417603
daiwk <daiwk@foxmail.com>
2019-10-29 01:42:44 +0800

    x

--- python/dl_frame/infer/test.py
+++ python/dl_frame/infer/test.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 #encoding=utf8
+
+"""
+testaaa
+"""
  
 
 class demoxxx(object):
@@ -12,5 +16,11 @@ class demoxxx(object):
             + kargs:dict
         Returns:
             xxx
+        '''
+        pass
+    def func_demo2():
+        '''
+        Args:
+            + m: int
         '''
         pass
