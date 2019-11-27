e9148ef7f4db7da3f2336e821983da08cb039324
daiwk <daiwk@foxmail.com>
2019-11-27 23:11:00 +0800

    add deepctr

--- demos/thirdparty/DeepCTR/examples/run_all.sh
+++ demos/thirdparty/DeepCTR/examples/run_all.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+python=~/tools/python-3-tf-2.0-gpu/bin/python3.6
+python=~/tools/python-3-tf-1.14.0-gpu/bin/python3.6
 function run_py(){
 
     code_path=./
@@ -7,7 +9,7 @@ function run_py(){
     do
       if [[ $file =~ .py ]]
         then
-          python $code_path$file
+          $python $code_path$file
           if [ $? -eq 0 ]
             then
               echo run $code_path$file succeed in $python_version
@@ -22,23 +24,23 @@ function run_py(){
 }
 
 ## python3
-python_version=python3
-source activate base
+python_version=$python
+##source activate base
 cd ..
-python setup.py install
+$python setup.py install
 cd ./examples
 run_py
 
-#python2
-python_version=python2
-source activate py27
-cd ..
-python setup.py install
-cd ./examples
-run_py
-echo "all examples run succeed in python2.7"
+###python2
+##python_version=python2
+##source activate py27
+##cd ..
+##python setup.py install
+##cd ./examples
+##run_py
+##echo "all examples run succeed in python2.7"
 
 
 echo "all examples run succeed in python3.6"
 
-echo "all examples run succeed in python2.7 and python3.6"
+##echo "all examples run succeed in python2.7 and python3.6"
