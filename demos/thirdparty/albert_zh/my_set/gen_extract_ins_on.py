9b85f932c5e97b723109c8973d3a06473c1794f7
daiwk <daiwk@foxmail.com>
2020-03-08 03:59:21 +0800

    extract

--- demos/thirdparty/albert_zh/my_set/gen_extract_ins_on.py
+++ demos/thirdparty/albert_zh/my_set/gen_extract_ins_on.py
@@ -8,7 +8,7 @@ Date: 2020-03-07 23:10:18
 """
 
 file = "./feed_data/tag_news_with_video_vector"
-file = "./feed_data/tag_news_with_video_vector.head"
+#file = "./feed_data/tag_news_with_video_vector.head"
 
 fin = open(file, 'r')
 file_ins = "./test_extract.txt"
@@ -18,4 +18,8 @@ with open(file_ins, 'w') as fout_ins:
     line = line.strip("\n")
     vals = line.split(" ")
     tag = vals[0]
+    try:
+      tag.decode("utf8")
+    except:
+      continue
     fout_ins.write(tag + "\t\t1\n")
