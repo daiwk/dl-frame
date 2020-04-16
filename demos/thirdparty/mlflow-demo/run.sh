a4263cb784e05b0cf2cd7201388f04790784aada
daiwk <daiwk@foxmail.com>
2020-04-17 02:53:58 +0800

    add mlflow

--- demos/thirdparty/mlflow-demo/run.sh
+++ demos/thirdparty/mlflow-demo/run.sh
@@ -1,11 +1,15 @@
+ps aux | grep "mlflow"| awk '{print $2}'| xargs kill -9 
+
 nohup mlflow ui &
 
 
-mlflow run ./ -P alpha=0.4 --no-conda
+mlflow run ./ -P alpha=0.4 --no-conda > train.log  2>&1
 
-run_id=c7dc4803b062493ba9e003569a30a1b3
+run_id=`tail -n 1 train.log|awk -F"'" '{print $2}'`
 
-nohup mlflow models serve -m runs:/c7dc4803b062493ba9e003569a30a1b3/model --port 3333 --no-conda &
+nohup mlflow models serve -m runs:/$run_id/model --port 3333 --no-conda &
+
+sleep 10
 
 curl -d '{"columns":["fixed acidity","volatile acidity"], "data":[[1,1]]}' -H 'Content-Type: application/json; format=pandas-split' -X POST localhost:3333/invocations
 
