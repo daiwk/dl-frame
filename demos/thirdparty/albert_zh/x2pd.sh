8382f575f6ac9374966050108edef80ab121b239
daiwk <daiwk@foxmail.com>
2020-05-29 16:53:13 +0800

    x

--- demos/thirdparty/albert_zh/x2pd.sh
+++ demos/thirdparty/albert_zh/x2pd.sh
@@ -1,2 +1,5 @@
-/home/work/tools/python-3-tf-1.14.0-gpu/bin/x2paddle -f tensorflow -m ./albert_dwk.pb -s pd-model --without_data_format_optimization
+####/home/work/tools/python-3-tf-1.14.0-gpu/bin/x2paddle -f tensorflow -m ./albert_dwk.pb -s pd-model --without_data_format_optimization
 #/home/work/tools/python-3-tf-1.14.0-gpu/bin/x2paddle -f tensorflow -m ./albert_dwk_origin.pb -s pd-model-origin --without_data_format_optimization
+
+
+/home/work/tools/python-3-tf-1.14.0-gpu/bin/x2paddle -f tensorflow -m ./albert_dwk.pb -s pd-model-merge --without_data_format_optimization --params_merge
